ddb8c7c56b6f09b3a99669d3efb21eed941ac7b1
a.trofimov <a.trofimov@owen.ru>
2023-12-22 12:47:26 +0300

    убрал ошибку в структуре

--- CheckDateTimeValidity.cpp
+++ CheckDateTimeValidity.cpp
@@ -8,63 +8,63 @@ struct DateTime
     int second;
 
     DateTime &ValidateYear() {
-        if (dt.year < 1) {
+        if (year < 1) {
             throw domain_error("year is too small"s);
         }
-        if (dt.year > 9999) {
+        if (year > 9999) {
             throw domain_error("year is too big"s);
         }
         return *this;
     }
 
     DateTime &ValidateMonth() {
-        if (dt.month < 1) {
+        if (month < 1) {
             throw domain_error("month is too small"s);
         }
-        if (dt.month > 12) {
+        if (month > 12) {
             throw domain_error("month is too big"s);
         }
         return *this;
     }
 
     DateTime &ValidateDay() {
-        const bool is_leap_year = (dt.year % 4 == 0) && !(dt.year % 100 == 0 && dt.year % 400 != 0);
+        const bool is_leap_year = (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
         const array month_lengths = {31, 28 + is_leap_year, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        if (dt.day < 1) {
+        if (day < 1) {
             throw domain_error("day is too small"s);
         }
-        if (dt.day > month_lengths[dt.month - 1]) {
+        if (day > month_lengths[month - 1]) {
             throw domain_error("day is too big"s);
         }
         return *this;
     }
 
     DateTime &ValidateHour() {
-        if (dt.hour < 0) {
+        if (hour < 0) {
             throw domain_error("hour is too small"s);
         }
-        if (dt.hour > 23) {
+        if (hour > 23) {
             throw domain_error("hour is too big"s);
         }
         return *this;
     }
 
     DateTime &ValidateMinute() {
-        if (dt.minute < 0) {
+        if (minute < 0) {
             throw domain_error("minute is too small"s);
         }
-        if (dt.minute > 59) {
+        if (minute > 59) {
             throw domain_error("minute is too big"s);
         }
         return *this;
     }
 
     DateTime &ValidateSecond() {
-        if (dt.second < 0) {
+        if (second < 0) {
             throw domain_error("second is too small");
         }
-        if (dt.second > 59) {
+        if (second > 59) {
             throw domain_error("second is too big"s);
         }
         return *this;
